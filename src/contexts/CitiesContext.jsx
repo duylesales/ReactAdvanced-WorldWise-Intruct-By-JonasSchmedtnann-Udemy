8bac71f916ae0790f4b0a91f481a8f473e097ef5
Duy Node <duynode@Duys-MBP.lan>
2023-11-15 00:16:41 +0700

    Creating a New City

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -38,6 +38,26 @@ function CitiesProvider({ children }) {
     }
   }
 
+  async function createCity(newCity) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${URL}/cities`,{
+        method:"POST",
+        body:JSON.stringify(newCity),
+        headers: {
+          "Content-Type":"application/json",
+        },
+      });
+      const data = await res.json();
+      setCities(cities=>[...cities,data]);
+      
+    } catch {
+      alert("An error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
@@ -45,6 +65,7 @@ function CitiesProvider({ children }) {
         isLoading,
         currentCity,
         getCity,
+        createCity,
       }}
     >
       {children}
